8b00868da336fa3bb32789217a1a167f4a660b8f
Lova-Nasaina <nasainalova05@gmail.com>
2025-10-05 22:51:47 +0200

    Loading Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
+import './index.css'
 import { useTranslation } from 'react-i18next'
+import { LoadingScreen } from './components/LoadingScreen';
+import { useState } from 'react';
 
 function App() {
   const { t } = useTranslation();
+  const [isLoaded, setIsLoaded] = useState(false);
 
 
   return (
     <>
-      <h1>Portfolio</h1>
-      <h2>{ t("greeting") }</h2>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{" "}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-grey-100`}>
+
+      </div>
     </>
   )
 }
